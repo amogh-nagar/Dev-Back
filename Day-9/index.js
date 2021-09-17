ae7aa48b505ade5ec26061b369fce2ddd1cf1827
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-09-17 11:43:10 +0530

    Day-9

--- Day-9/index.js
+++ Day-9/index.js
@@ -11,9 +11,21 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname,'public','test.json'));
   //   res.download(path.join(__dirname,'public','test.json'),'hello.json')
-  res.render("index", { title: "Express" });
+  //   res.render("index", { title: "Express" });
+  console.log(Date.now());
+  res
+    .status(200)
+    .cookie("token", "test", {
+      expire: new Date(Date.now() + 3600000),
+    })
+    .cookie("hello", "hello").send("Admin")
+    // .redirect(301, "/admin");
 });
 
-app.listen(5050,()=>{
-    console.log('Connected');
+// app.get("/admin", (req, res, next) => {
+//   res.status(200).send("Admin");
+// });
+
+app.listen(5050, () => {
+  console.log("Connected");
 });
